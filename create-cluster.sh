f931e1a0b6316135dcba5a3de0ad5529df388539
Amin Al Ali Al Darwish <amin@bunnyshell.com>
2023-08-18 14:52:47 +0400

    fixes all around

--- create-cluster.sh
+++ create-cluster.sh
@@ -14,10 +14,10 @@ fi
 echo "🐰 Checking gcloud project configuration..."
 project_id=$(gcloud config get-value project)
 
-echo "Setting up project..."
+echo "🐰 Setting up project..."
 # Check if gcloud is initiated, if not, run the first command
 if ! gcloud config get-value project &>/dev/null; then
-    echo "Initializing gcloud... ⚡"
+    echo "🐰 Initializing gcloud... ⚡"
     gcloud init --console-only
 fi
 
@@ -27,18 +27,18 @@ read -p "🐰 Do you want to create a new project? (y/n): " create_new
 if [ "$create_new" == "y" ]; then
     read -p "🐰 Enter the name for your new project: " new_project_name
     new_project_name_snake=$(echo "$new_project_name" | sed -E 's/ /_/g' | tr '[:upper:]' '[:lower:]')
-    echo "Creating a new project: $new_project_name_snake..."
+    echo "🐰 Creating a new project: $new_project_name_snake..."
     gcloud projects create "$new_project_name_snake" --name="$new_project_name_snake"
     gcloud config set project "$new_project_name_snake"
 fi
 
 if [ "$create_new" == "n" ]; then
     read -p "🐰 Enter the existing project ID: " existing_project_id
-    echo "Using existing project: $existing_project_id..."
+    echo "🐰 Using existing project: $existing_project_id..."
     gcloud config set project "$existing_project_id"
 fi
 
-echo "Enabling GKE services in the project..."
+echo "🐰 Enabling GKE services in the project..."
 gcloud services enable container.googleapis.com
 
 # Input cluster name, region, and node size
@@ -46,21 +46,20 @@ read -p "🐰 Enter a name for your GKE cluster: " cluster_name
 read -p "🐰 Enter the region for your GKE cluster (e.g., us-central1): " region
 read -p "🐰 Enter the desired node size for your GKE cluster (e.g., n1-standard-2): " node_size
 
-echo "Creating a GKE cluster..."
-gcloud container clusters create "$cluster_name" --region "$region" --node-locations "$region-a" --machine-type "$node_size"
+echo "🐰 Creating a GKE cluster..."
+gcloud container clusters create "$cluster_name" --region "$region" --machine-type "$node_size"
 
-echo "Getting cluster configuration..."
-# ... (previous steps)
 
-echo "Retrieving the Service Account..."
+echo "🐰 Retrieving the Service Account..."
 # Check if BunnyShell service account already exists
 service_account_name="bunnyshell-access"
 existing_service_account_email="$service_account_name@$project_id.iam.gserviceaccount.com"
-if ! gcloud iam service-accounts list --filter="email:$existing_service_account_email" --format="value(email)" &>/dev/null; then
-    echo "🐰 Creating the $service_account_name Service Account..."
-    gcloud iam service-accounts create "$service_account_name"
-else
+
+if [ "$(gcloud iam service-accounts list --filter="email:$existing_service_account_email" --format="value(email)")" = "$existing_service_account_email" ]; then
     echo "🐰 $service_account_name Service Account ($existing_service_account_email) already exists."
+else
+    echo "🐰 Creating the $service_account_name Service Account..."
+    gcloud iam service-accounts create "$service_account_name" --display-name="$service_account_name"
 fi
 
 echo "🐰 Granting the Service Account access to your cluster..."
@@ -78,6 +77,7 @@ gcloud iam service-accounts keys create gsa-key.json \
 echo "🐰 Downloading Google Service Account Key."
 gcloud iam service-accounts keys download gsa-key.json \
     --iam-account="$service_account_name@$project_id.iam.gserviceaccount.com"
+
 # Display additional information
 current_context=$(kubectl config current-context)
 cloud_region=$(kubectl config get-contexts "$current_context" | awk '{print $3}' | tail -n 1)
@@ -86,7 +86,7 @@ certificate=$(kubectl config view --raw --minify --flatten -o jsonpath='{.cluste
 
 
 echo -e "\n🐰 Additional information:"
-echo "🐰 GKE Cluster Name: $cluster_name_gcloud"
+echo "🐰 GKE Cluster Name: $cluster_name"
 echo "🐰 Cluster URL: $cluster_url"
 echo "🐰 Cloud Region: $region"
 echo "🐰 Service Account ID: $service_account_name@$project_id.iam.gserviceaccount.com"
